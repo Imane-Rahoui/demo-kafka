a174b8748f0de05d3439919e71b2f4ba7a92f9f1
Imane-Rahoui <Imaanerahoui@gmail.com>
2023-01-24 21:33:21 +0100

    function stream processing & courbe done

--- src/main/java/org/sid/demospringcloudstreamskafka/services/PageEventService.java
+++ src/main/java/org/sid/demospringcloudstreamskafka/services/PageEventService.java
@@ -1,9 +1,16 @@
 package org.sid.demospringcloudstreamskafka.services;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.sid.demospringcloudstreamskafka.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -39,6 +46,30 @@ public class PageEventService {
             input.setName("Page:"+input.getName().length());
             input.setUser("User=>1");
             return input;
+        };
+    }
+    @Bean
+    public Function<KStream<String,PageEvent>, KStream<String,Long>> kStreamFunction(){
+        return (input)->{
+            // on va lire du topic R2 pcq le supplier ecrit dans ce topic
+            // chaque enregistrement s'appelle un data record
+            // il y'a 2 partie : key et value
+            // pour le moment key = null pcq on envoie des json comme value
+            // on va produire en sortie des statistiques
+            // exp la PAGE a ete visité combien de fois
+            // un ktable : est le resultat
+            return input
+                    .filter((k,v)->v.getDuration()>100)// filtrer data
+                    .map((k,v)->new KeyValue<>(v.getName(),0L))// la cle mtn c est le nom de la page et la val c'est 0
+                    .groupBy((k,v)->k, Grouped.with(Serdes.String(),Serdes.Long()))// la clé va etre deserialisé et serialisé en format String et la valeur long - groupBy return un stream
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5))) // statistique sur les 5 derniers cas - windowedBy : produit un ktable c pour ca qu on va ajouter to stream et map pour organiser
+                    .count(Materialized.as("page-count"))
+                    //vue materializé : store qui s appel page-cont : on peut recuperer ces donnees et les afficher dans une application web
+                    // page-cont est un store ou ktable ou table
+                    // grouper par rapport a la clé
+                    .toStream() // le res est un stream
+                    .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+":"+k.key(),v)); // pour avoir cet affichage =>2023-01-24T19:32:45Z2023-01-24T19:32:50Z:P1
+                        // on voit l'évolution et non pas le cumul
         };
     }
 }
--- src/main/java/org/sid/demospringcloudstreamskafka/web/PageEventRestController.java
+++ src/main/java/org/sid/demospringcloudstreamskafka/web/PageEventRestController.java
@@ -1,13 +1,26 @@
 package org.sid.demospringcloudstreamskafka.web;
 
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
+import org.apache.kafka.streams.state.WindowStoreIterator;
 import org.sid.demospringcloudstreamskafka.entities.PageEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @RestController
@@ -15,6 +28,8 @@ public class PageEventRestController {
 
     @Autowired
     private StreamBridge streamBridge; // ça fonctionne vc ts les brokers
+    @Autowired
+    private InteractiveQueryService interactiveQueryService;
 
     @GetMapping("/publish/{topic}/{name}")
     public PageEvent publish(@PathVariable String topic, @PathVariable String name) {
@@ -26,4 +41,56 @@ public class PageEventRestController {
     // pr le consumer il y'a deux facons :
     // @messagelistener de spring cloud stream ms une version deprecié pcq il y a mieux
     // utiliser la programmation fonctionnelle (qu on va utilisé)
+
+    //une fois le client conn - le serveur va passer les données au client
+    // flux : un object qui va etre converti en format json
+    // clé : nom page et value le nbr
+
+    // visualiser les resultats  : http://localhost:8080/analytics
+    @GetMapping(path = "/analyticsfi/{page}",produces = MediaType.TEXT_EVENT_STREAM_VALUE) // produces pr dire je vais retouner ce type
+    public Flux<Map<String, Long>>  analyticsFiltered(@PathVariable String page){
+        return Flux.interval(Duration.ofSeconds(1)) // genere moi un stream et un enregistrement dans ce stream chaque seconde
+                .map(sequence->{
+                    // pratiqu ms vous pouvez utilisez ksql pr ecrire avec du sql
+                    Map<String,Long> stringLongMap=new HashMap<>();
+                    // interoger le store page-count | on doit specifier le type de store dans ce cas on a utilisé de type windows sinon keyValueStore()
+                    ReadOnlyWindowStore<String, Long> windowStore = interactiveQueryService.getQueryableStore("page-count", QueryableStoreTypes.windowStore());
+                    Instant now=Instant.now();
+                    Instant from=now.minusMillis(5000);
+                    // fetchAll : comme un select le calcule des 5 derniers secondes | mn db - 5s l db
+                    //KeyValueIterator<Windowed<String>, Long> fetchAll = windowStore.fetchAll(from, now);
+                    WindowStoreIterator<Long> fetchAll = windowStore.fetch(page, from, now); // si vous voulez par des criteres exp le res de la page P1
+                    while (fetchAll.hasNext()){
+                        KeyValue<Long, Long> next = fetchAll.next();
+                        /*KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                        // l'enregistrement qu'on va return
+                        //stringLongMap.put(next.key.window().startTime(),next.value); // par exp d'autres params
+                        stringLongMap.put(next.key.key(),next.value);*/
+                        stringLongMap.put(page, next.value);
+                    }
+                    return stringLongMap;
+                }).share(); // le stream est partagé par plusieurs users sinon chaque user recoit son propre flux
+    }
+
+    @GetMapping(path = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE) // produces pr dire je vais retouner ce type
+    public Flux<Map<String, Long>>  analytics(){
+        return Flux.interval(Duration.ofSeconds(1)) // genere moi un stream et un enregistrement dans ce stream chaque seconde
+                .map(sequence->{
+                    Map<String,Long> stringLongMap=new HashMap<>();
+                    // interoger le store page-count | on doit specifier le type de store dans ce cas on a utilisé de type windows sinon keyValueStore()
+                    ReadOnlyWindowStore<String, Long> windowStore = interactiveQueryService.getQueryableStore("page-count", QueryableStoreTypes.windowStore());
+                    Instant now=Instant.now();
+                    Instant from=now.minusMillis(5000);
+                    // fetchAll : comme un select le calcule des 5 derniers secondes | mn db - 5s l db
+                    KeyValueIterator<Windowed<String>, Long> fetchAll = windowStore.fetchAll(from, now);
+                    //WindowStoreIterator<Long> fetchAll = windowStore.fetch("P1", from, now); // si vous voulez par des criteres exp le res de la page P1
+                    while (fetchAll.hasNext()){
+                        KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                        // l'enregistrement qu'on va return
+                        //stringLongMap.put(next.key.window().startTime(),next.value); // par exp d'autres params
+                        stringLongMap.put(next.key.key(),next.value);
+                    }
+                    return stringLongMap;
+                }).share(); // le stream est partagé par plusieurs users sinon chaque user recoit son propre flux
+    }
 }
